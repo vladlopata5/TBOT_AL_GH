39c96a0a93f778d3f4705c06a4edd62414cbf81d
vladlopata5 <vladlopata5@gmail.com>
2024-05-05 17:24:14 +0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#testzalivki
+import random
+import psycopg2
+import telebot
+from telebot import types
+from collections import Counter
+
+bot = telebot.TeleBot('6889928482:AAGEDFlbcsLv9_r9Hjch6I6HtaXpPfxbUWY')
+
+
+
+
+
+
+
+
+
+
+
+bot.infinity_polling(none_stop=True)
